5e97b03d56bebf8aedfe2b2a7ce2e7c3aa9222bb
cranberrymuffin <aparnalovestocode@gmail.com>
2025-01-11 13:36:28 -0500

    react three fiber point cloud with snowflake texture mapping

--- src/Experience.js
+++ src/Experience.js
@@ -1,15 +1,30 @@
 import { OrbitControls } from "@react-three/drei"
+import { useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three'
 
 export default function Experience()
 {
+    const colorMap = useLoader(TextureLoader, '/textures/snowflake.jpg')
+
+    const count = 5000
+    const positions = new Float32Array(count * 3)
+
+    for(let i = 0; i < count * 3; i++) {
+        positions[i] = (Math.random() - 0.5) * 10
+    }
+
     return <group> 
         <OrbitControls/>
-        <mesh>
-            <boxGeometry />
-            <meshBasicMaterial color="red"/>
-        </mesh>
+        <points>
+            <bufferGeometry >
+                <bufferAttribute 
+                attach="attributes-position"
+                count = {count}
+                itemSize = {3}
+                array={positions}
+                />
+            </bufferGeometry>
+            <pointsMaterial size={0.1} sizeAttenuation={true} map={colorMap} />
+        </points>
     </group>
 }
-
-// Render updates for free
-// Camera positioning and intrinsics for free
